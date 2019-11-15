89d07d19c0de82f4d612b80cc9f086e3ee833d96
Mikhail Bogdan <misha.bogdan.2000@mail.ru>
2019-11-16 00:33:50 +0300

    init

---
+++ projectTypes.py
@@ -0,0 +1,9 @@
+users = []
+
+class User:
+    def __init__(self, username, password, firstname, lastname, age):
+        self.username = username
+        self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
